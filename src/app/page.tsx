00c679f591643d9e9db11ce9dcc44ec323fdb373
sikfar <sikander.farooq93@gmail.com>
2023-10-23 12:17:56 +0200

    doc: Added notes to README

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 'use client'
 
 import { useEffect, useRef, useState } from 'react'
-import { FlightController, getDeviceProfile, View, } from "@novorender/api";
-import { createAPI, type SceneData, SceneLoadFail } from "@novorender/data-js-api";
-
+import { FlightController, View, } from "@novorender/api";
+import { type SceneData, SceneLoadFail } from "@novorender/data-js-api";
 import './page.css';
 import Panel from "@/ui/panel/panel";
+import { initializeCanvas } from "@/service/search-object";
 
 
 export default function Home() {
@@ -22,48 +22,12 @@ export default function Home() {
 
 
 	async function main(canvas: HTMLCanvasElement | null) {
-		const gpuTier = 2; // laptop with reasonably new/powerful GPU.
-		// Get Device Profile
-		const deviceProfile = getDeviceProfile(gpuTier);
-		const baseUrl = new URL("/novorender/api/", location.origin);
-		const imports = await View.downloadImports({baseUrl}); // or whereever you copied the public/ files from the package.
-		// Create a View
-		const view = new View(canvas as HTMLCanvasElement, deviceProfile, imports);
-		setView(view)
-
-		// load a predefined environment to set it as background
-		const envIndexUrl = new URL("https://api.novorender.com/assets/env/index.json");
-		const envs = await view.availableEnvironments(envIndexUrl);
-		// modify the render state
-		const dataApi = createAPI({
-			serviceUrl: "https://data.novorender.com/api",
-		});
-
-		// Load scene metadata
-		// Condos scene ID, but can be changed to any public scene ID
-		const sceneData = await dataApi.loadScene("95a89d20dd084d9486e383e131242c4c");
-
-		setSceneData(sceneData)
-		// Destructure relevant properties into variables
-		const {url} = sceneData as SceneData;
-		// load the scene using URL gotten from `sceneData`
-		const config = await view.loadSceneFromURL(new URL(url));
-		const {center, radius} = config.boundingSphere;
-		view.activeController.autoFit(center, radius);
-		const flightController = await view.switchCameraController("flight");
-
-		setController(flightController)
-
-		// run the view
-		await view.run();
-		// dispose-off GPU resources
-		view.dispose();
+		await initializeCanvas(canvas, setView, setSceneData, setController);
 	}
 
 
 	return (
 		<main className='canvas-container'>
-			{/*TODO: MOVE TO CSS*/}
 			<canvas ref={canvas}
 			        style={{width: '100%', height: '100%', background: 'grey'}}>
 			</canvas>
--- src/service/search-object.ts
+++ src/service/search-object.ts
@@ -1,5 +1,12 @@
-import { createNeutralHighlight, type RenderStateHighlightGroups, type View } from "@novorender/api";
-import { type SceneData } from "@novorender/data-js-api";
+import {
+	BuiltinCameraControllerType,
+	createNeutralHighlight,
+	FlightController,
+	getDeviceProfile,
+	type RenderStateHighlightGroups,
+	View,
+} from "@novorender/api";
+import { createAPI, type SceneData, SceneLoadFail } from "@novorender/data-js-api";
 
 export const searchObject = async (query: string, view: View, sceneData: SceneData, signal: AbortSignal): Promise<void> => {
 	try {
@@ -22,4 +29,31 @@ export const searchObject = async (query: string, view: View, sceneData: SceneDa
 	} catch (e) {
 		console.warn(e);
 	}
+}
+
+export const initializeCanvas = async (canvas: HTMLCanvasElement | null, setView: (value: (((prevState: (View<BuiltinCameraControllerType, Extract<keyof BuiltinCameraControllerType, string>> | undefined)) => (View<BuiltinCameraControllerType, Extract<keyof BuiltinCameraControllerType, string>> | undefined)) | View<BuiltinCameraControllerType, Extract<keyof BuiltinCameraControllerType, string>> | undefined)) => void, setSceneData: (value: (((prevState: (SceneData | SceneLoadFail | undefined)) => (SceneData | SceneLoadFail | undefined)) | SceneData | SceneLoadFail | undefined)) => void, setController: (value: (((prevState: (FlightController | undefined)) => (FlightController | undefined)) | FlightController | undefined)) => void): Promise<void> => {
+	const gpuTier = 2;
+	const deviceProfile = getDeviceProfile(gpuTier);
+	const baseUrl = new URL("/novorender/api/", location.origin);
+	const imports = await View.downloadImports({baseUrl});
+	const view = new View(canvas as HTMLCanvasElement, deviceProfile, imports);
+	setView(view)
+
+	const dataApi = createAPI({
+		serviceUrl: "https://data.novorender.com/api",
+	});
+
+	const sceneData = await dataApi.loadScene("YOUR API KEY HERE");
+
+	setSceneData(sceneData)
+	const {url} = sceneData as SceneData;
+	const config = await view.loadSceneFromURL(new URL(url));
+	const {center, radius} = config.boundingSphere;
+	view.activeController.autoFit(center, radius);
+	const flightController = await view.switchCameraController("flight");
+
+	setController(flightController)
+
+	await view.run();
+	view.dispose();
 }
